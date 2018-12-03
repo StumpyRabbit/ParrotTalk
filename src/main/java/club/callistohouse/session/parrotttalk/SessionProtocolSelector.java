769f3de5a6d7df5dc27d55e32461465f9ec21447
RobWithers <robert.withers@pm.me>
2018-12-02 20:21:16 -0500

    clean up SessionProtocolSelector state machine.

--- src/main/java/club/callistohouse/session/parrotttalk/SessionProtocolSelector.java
+++ src/main/java/club/callistohouse/session/parrotttalk/SessionProtocolSelector.java
@@ -14,20 +14,19 @@ import club.callistohouse.session.marshmuck.State;
 import club.callistohouse.session.marshmuck.Trigger;
 import club.callistohouse.session.multiprotocol_core.DuplicateConnection;
 import club.callistohouse.session.multiprotocol_core.Frame;
-import club.callistohouse.session.multiprotocol_core.InternalChangeEncryption;
 import club.callistohouse.session.multiprotocol_core.MAC;
 import club.callistohouse.session.multiprotocol_core.MessageEnum;
 import club.callistohouse.session.multiprotocol_core.NotMe;
 import club.callistohouse.session.multiprotocol_core.PhaseHeader;
 import club.callistohouse.session.multiprotocol_core.ProtocolAccepted;
 import club.callistohouse.session.multiprotocol_core.ProtocolOffered;
-import club.callistohouse.session.parrotttalk.Session.Identified;
 import club.callistohouse.session.rendezvous_v3_6.GiveInfo;
 import club.callistohouse.session.rendezvous_v3_6.Go;
 import club.callistohouse.session.rendezvous_v3_6.GoToo;
 import club.callistohouse.session.rendezvous_v3_6.IAm;
 import club.callistohouse.session.rendezvous_v3_6.IWant;
 import club.callistohouse.session.rendezvous_v3_6.ReplyInfo;
+import club.callistohouse.session.rendezvous_v3_7.Hello_v3_7;
 import club.callistohouse.session.thunkstack_core.ThunkFinishedException;
 import club.callistohouse.session.thunkstack_core.ThunkLayer;
 import club.callistohouse.session.thunkstack_core.ThunkStack;
@@ -103,77 +102,28 @@ public class SessionProtocolSelector extends ThunkLayer {
 			e.printStackTrace();
 		}
 	}
-	void sendIAm() {
-		PhaseHeader header = new IAm(getLocalIdentity());
-		securityOps.addLocalFrame(header.toFrame());
-		stateMachine.fire(Trigger.ExpectGiveInfo);
+	void sendHello_v3_7() {
+		PhaseHeader header;
 		try {
+			header = new Hello_v3_7(getRemoteIdentity().getVatId(), getRemoteIdentity().getDomain(), getLocalIdentity().getPublicKey(), securityOps.getDataEncoders(), securityOps.getCryptoProtocols(), securityOps.getDhParam(), session.getNearKey().getSignatureBytes(securityOps.getLocalMessagesBytes()));
+			securityOps.addLocalFrame(header.toFrame());
+			stateMachine.fire(Trigger.ExpectIAm);
 			send(header);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	void sendGiveInfo() {
-		PhaseHeader header = new GiveInfo(getLocalIdentity());
-		securityOps.addLocalFrame(header.toFrame());
-		stateMachine.fire(Trigger.ExpectReplyInfo);
-    	try {
-			send(header);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	void sendReplyInfo() {
-		PhaseHeader header = new ReplyInfo(securityOps.getSessionAgentMap().getProtocolNames(), securityOps.getSessionAgentMap().getDataEncoderNames());
-		securityOps.addLocalFrame(header.toFrame());
-		stateMachine.fire(Trigger.ExpectGo);
-		try {
-			send(header);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	void sendGo() {
-		try {
-			byte[] signature = null;
-			byte[] dhParam = securityOps.getDhParam();
-			Go go = new Go(securityOps.map.getSelectedProtocolName(), securityOps.map.getSelectedEncoderName(), dhParam, new byte[0]);
-			securityOps.addLocalFrame(go.toFrame());
-			byte[] msgBytes = securityOps.getLocalMessagesBytes();
-			try {
-				signature = getLocalIdentity().getSignatureBytes(msgBytes);
-			} catch (SignatureException e) {
-				e.printStackTrace();
-				stateMachine.fire(Trigger.SendBye);
-				return;
-			}
-			go.setSignature(signature);
-	    	send(go);
-			stateMachine.fire(Trigger.ExpectGoToo);
 		} catch (NoSuchAlgorithmException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (SignatureException e1) {
+			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	void sendGoToo() {
+	void sendDuplicateConnection() {
+		stateMachine.fire(Trigger.Disconnect);
 		try {
-			byte[] signature = null;
-			byte[] dhParam = securityOps.getDhParam();
-			GoToo goToo = new GoToo(securityOps.map.getSelectedProtocolName(), securityOps.map.getSelectedEncoderName(), dhParam, new byte[0]);
-			securityOps.addLocalFrame(goToo.toFrame());
-			byte[] msgBytes = securityOps.getLocalMessagesBytes();
-			try {
-				signature = getLocalIdentity().getSignatureBytes(msgBytes);
-			} catch (SignatureException e) {
-				e.printStackTrace();
-				stateMachine.fire(Trigger.SendBye);
-				return;
-			}
-			goToo.setSignature(signature);
-	    	send(goToo);
-		} catch (NoSuchAlgorithmException e1) {
-			e1.printStackTrace();
+			send(new DuplicateConnection());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -192,20 +142,10 @@ public class SessionProtocolSelector extends ThunkLayer {
 			handleMessage((ProtocolOffered) header);
 		} else if(ClassUtil.isAssignableFrom(header, ProtocolAccepted.class)) {
 			handleMessage((ProtocolAccepted) header);
-		} else if(ClassUtil.isAssignableFrom(header, MAC.class)) {
-			handleMessage((MAC) header);
 		} else if(ClassUtil.isAssignableFrom(header, IWant.class)) {
 			handleMessage((IWant) header);
-		} else if(ClassUtil.isAssignableFrom(header, IAm.class)) {
-			handleMessage((IAm) header);
-		} else if(ClassUtil.isAssignableFrom(header, GiveInfo.class)) {
-			handleMessage((GiveInfo) header);
-		} else if(ClassUtil.isAssignableFrom(header, ReplyInfo.class)) {
-			handleMessage((ReplyInfo) header);
-		} else if(ClassUtil.isAssignableFrom(header, Go.class)) {
-			handleMessage((Go) header);
-		} else if(ClassUtil.isAssignableFrom(header, GoToo.class)) {
-			handleMessage((GoToo) header);
+		} else if(ClassUtil.isAssignableFrom(header, Hello_v3_7.class)) {
+			handleMessage((Hello_v3_7) header);
 		} else if(ClassUtil.isAssignableFrom(header, DuplicateConnection.class)) {
 			handleMessage((DuplicateConnection) header);
 		} else if(ClassUtil.isAssignableFrom(header, NotMe.class)) {
@@ -235,14 +175,6 @@ public class SessionProtocolSelector extends ThunkLayer {
     		throw new RuntimeException("Terminal in wrong connection state for ReceivedProtocolAccepted trigger; in state: " + stateMachine.getState() + "; expecting: CallReceiveProtocolAccepted");
     	}
 	}
-	public void handleMessage(MAC body) {
-    	if(stateMachine.isInState(State.EncryptedConnected)) {
-    		stack.upcall(body.toFrame(), this);
-    	} else {
-    		log.debug("Terminal in wrong connection state for EncryptedConnected trigger; in state: " + stateMachine.getState());
-    		throw new RuntimeException("Terminal in wrong connection state for ReceivedProtocolAccepted trigger; in state: " + stateMachine.getState() + "; expecting: CallReceiveProtocolAccepted");
-    	}
-	}
 
 	public void handleMessage(IWant body) {
 		securityOps.addRemoteFrame(body.toFrame());
@@ -259,126 +191,19 @@ public class SessionProtocolSelector extends ThunkLayer {
     		throw new RuntimeException("Terminal in wrong connection state for IWant msg; in state: " + stateMachine.getState() + "; expecting: StartupReceiveIWant");
     	}
 	}
-	public void handleMessage(IAm body) {
+	public void handleMessage(Hello_v3_7 body) {
 		securityOps.addRemoteFrame(body.toFrame());
-    	if(stateMachine.isInState(State.StartupReceiveIAm)) {
-    		isIncoming = false;
-    		securityOps.setIsIncoming(false);
-    		getRemoteIdentity().setVatId(body.getVatId());
-    		getRemoteIdentity().setPublicKey(body.getPublicKeyImpl());
-    		session.fire(new Identified());
-    		stateMachine.fire(Trigger.ReceivedIAm);
-    	} else {
-    		log.debug("Terminal in wrong connection state for IAm msg; in state: " + stateMachine.getState() + "; expecting: StartupReceiveIAm");
-    		throw new RuntimeException("Terminal in wrong connection state for IAm msg; in state: " + stateMachine.getState() + "; expecting: StartupReceiveIAm");
-    	}
-	}
-	public void handleMessage(GiveInfo body) {
-		securityOps.addRemoteFrame(body.toFrame());
-    	if(stateMachine.isInState(State.StartupReceiveGiveInfo)) {
-    		getRemoteIdentity().setVatId(body.getVatId());
-    		getRemoteIdentity().setPublicKey(body.getPublicKeyImpl());
-    		session.fire(new Identified());
-    		stateMachine.fire(Trigger.ReceivedGiveInfo);
-    	} else {
-    		log.debug("Terminal in wrong connection state for GiveInfo msg; in state: " + stateMachine.getState() + "; expecting: StartupReceiveGiveInfo");
-    		throw new RuntimeException("Terminal in wrong connection state for GiveInfo msg; in state: " + stateMachine.getState() + "; expecting: StartupReceiveGiveInfo");
-    	}
-	}
-	public void handleMessage(ReplyInfo body) {
-		securityOps.addRemoteFrame(body.toFrame());
-    	if(stateMachine.isInState(State.IdentifiedStartupReceiveReplyInfo)) {
-			securityOps.map.setSelectedProtocolName(securityOps.matchBestCryptoProtocol(body.getCryptoProtocols()));
-			securityOps.map.setSelectedEncoderName(securityOps.matchBestDataEncoder(body.getDataEncoders()));
-
-    		stateMachine.fire(Trigger.ReceivedReplyInfo);
-    	} else {
-    		log.debug("Terminal in wrong connection state for ReplyInfo msg; in state: " + stateMachine.getState() + "; expecting: IdentifiedStartupReceiveReplyInfo");
-    		throw new RuntimeException("Terminal in wrong connection state for ReplyInfo msg; in state: " + stateMachine.getState() + "; expecting: IdentifiedStartupReceiveReplyInfo");
-    	}
-	}
-	public void handleMessage(Go body) {
-    	if(stateMachine.isInState(State.IdentifiedStartupReceiveGo)) {
-    		byte[] sig = body.getSignature();
-    		body.setSignature(new byte[0]);
-    		try {
-				byte[] dhParam = securityOps.getDhParam();
-			} catch (NoSuchAlgorithmException e2) {
-				e2.printStackTrace();
-			}
-    		securityOps.addRemoteFrame(body.toFrame());
-    		try {
-    			getRemoteIdentity().verifySignature(securityOps.getRemoteMessagesBytes(), sig);
-    		} catch (SignatureException e) {
-    			stateMachine.fire(Trigger.SendBye);
-    			return;
+    	if(stateMachine.isInState(State.StartupReceiveIWant)) {
+    		isIncoming = true;
+    		securityOps.setIsIncoming(true);
+    		if(!body.getVatId().equals(getLocalIdentity().getVatId())) {
+    			stateMachine.fire(Trigger.SendNotMe);
+    		} else {
+    			stateMachine.fire(Trigger.ReceivedIWant);
     		}
-			try {
-				securityOps.processOtherSideDhParam(body.getDiffieHellmanParam(), isIncoming);
-				securityOps.map.setSelectedProtocolName(body.getCryptoProtocol());
-				securityOps.map.setSelectedEncoderName(body.getDataEncoder());
-			} catch (NoSuchAlgorithmException e1) {
-				e1.printStackTrace();
-			}
-    		stateMachine.fire(Trigger.ReceivedGo);
-    		try {
-    			startupSuccessful(isIncoming);
-    			SecurityOps secrets = securityOps.clone();
-    			secrets.makeNullLogging();
-    			session.fire(new InternalChangeEncryption(secrets));
-    		} catch (CloneNotSupportedException e) {
-    			e.printStackTrace();
-    		} catch (IOException e) {
-				e.printStackTrace();
-			}
-       	} else {
-    		log.debug("Terminal in wrong connection state for Go msg; in state: " + stateMachine.getState() + "; expecting: IdentifiedStartupReceiveGo");
-    		throw new RuntimeException("Terminal in wrong connection state for Go msg; in state: " + stateMachine.getState() + "; expecting: IdentifiedStartupReceiveGo");
-    	}
-	}
-
-	public void handleMessage(GoToo body) {
-    	if(stateMachine.isInState(State.IdentifiedStartupReceiveGoToo)) {
-    		byte[] sig = body.getSignature();
-    		body.setSignature(new byte[0]);
-    		try {
-				byte[] dhParam = securityOps.getDhParam();
-			} catch (NoSuchAlgorithmException e2) {
-	    		log.debug("NoSuchAlgorithmException");
-				e2.printStackTrace();
-			}
-    		securityOps.addRemoteFrame(body.toFrame());
-    		byte[] msgBytes = securityOps.getRemoteMessagesBytes();
-    		try {
-    			getRemoteIdentity().verifySignature(msgBytes, sig);
-    		} catch (SignatureException e) {
-    			stateMachine.fire(Trigger.SendBye);
-    			return;
-    		}
-    		try {
-    			securityOps.processOtherSideDhParam(body.getDiffieHellmanParam(), isIncoming);
-				securityOps.map.setSelectedProtocolName(body.getCryptoProtocol());
-				securityOps.map.setSelectedEncoderName(body.getDataEncoder());
-			} catch (NoSuchAlgorithmException e1) {
-	    		log.debug("NoSuchAlgorithmException");
-				e1.printStackTrace();
-			}
-    		stateMachine.fire(Trigger.ReceivedGoToo);
-    		try {
-    			startupSuccessful(isIncoming);
-    			SecurityOps secrets = securityOps.clone();
-    			secrets.makeNullLogging();
-    			session.fire(new InternalChangeEncryption(secrets));
-    		} catch (CloneNotSupportedException e) {
-	    		log.debug("CloneNotSupportedException in startupSuccessful");
-    			e.printStackTrace();
-    		} catch (IOException e) {
-	    		log.debug("IOException in startupSuccessful");
-				e.printStackTrace();
-			}
     	} else {
-    		log.debug("Terminal in wrong connection state for GoToo msg; in state: " + stateMachine.getState() + "; expecting: IdentifiedStartupReceiveGoToo");
-    		throw new RuntimeException("Terminal in wrong connection state for GoToo msg; in state: " + stateMachine.getState() + "; expecting: IdentifiedStartupReceiveGoToo");
+    		log.debug("Terminal in wrong connection state for IWant msg; in state: " + stateMachine.getState() + "; expecting: StartupReceiveIWant");
+    		throw new RuntimeException("Terminal in wrong connection state for IWant msg; in state: " + stateMachine.getState() + "; expecting: StartupReceiveIWant");
     	}
 	}
 	private void startupSuccessful(boolean isIncoming2) throws IOException {
@@ -445,28 +270,6 @@ public class SessionProtocolSelector extends ThunkLayer {
 		sessionConnectionConfig.configure(State.StartupReceiveIAm)
 			.substateOf(State.Startup)
 			.permit(Trigger.ReceivedIAm, State.StartupSendingGiveInfo);
-		sessionConnectionConfig.configure(State.StartupSendingGiveInfo)
-			.substateOf(State.Startup)
-			.onEntry(new Action() {
-				public void doIt() {
-					sendGiveInfo();
-				}})
-			.permit(Trigger.ExpectReplyInfo, State.IdentifiedStartupReceiveReplyInfo);
-		sessionConnectionConfig.configure(State.IdentifiedStartupReceiveReplyInfo)
-			.substateOf(State.IdentifiedStartup)
-			.permit(Trigger.ReceivedReplyInfo, State.IdentifiedStartupSendingGo);
-		sessionConnectionConfig.configure(State.IdentifiedStartupSendingGo)
-			.substateOf(State.IdentifiedStartup)
-			.onEntry(new Action() {
-				public void doIt() {
-					sendGo();
-				}})
-			.permit(Trigger.SendBye, State.IdentifiedStartupSendingBye)
-			.permit(Trigger.ExpectGoToo, State.IdentifiedStartupReceiveGoToo);
-		sessionConnectionConfig.configure(State.IdentifiedStartupReceiveGoToo)
-			.substateOf(State.IdentifiedStartup)
-			.permit(Trigger.SendBye, State.IdentifiedStartupSendingBye)
-			.permit(Trigger.ReceivedGoToo, State.IdentifiedStartupConnecting);
 		sessionConnectionConfig.configure(State.IdentifiedStartupConnecting)
 			.substateOf(State.IdentifiedStartup)
 			.onEntry(new Action() {
@@ -499,35 +302,6 @@ public class SessionProtocolSelector extends ThunkLayer {
 			.substateOf(State.Startup)
 			.permit(Trigger.SendNotMe, State.StartupSendingNotMe)
 			.permit(Trigger.ReceivedIWant, State.StartupSendingIAm);
-		sessionConnectionConfig.configure(State.StartupSendingIAm)
-			.substateOf(State.Startup)
-			.onEntry(new Action() {
-				public void doIt() {
-					sendIAm();
-				}})
-			.permit(Trigger.ExpectGiveInfo, State.StartupReceiveGiveInfo);
-		sessionConnectionConfig.configure(State.StartupReceiveGiveInfo)
-			.substateOf(State.Startup)
-			.permit(Trigger.ReceivedGiveInfo, State.IdentifiedStartupSendingReplyInfo);
-		sessionConnectionConfig.configure(State.IdentifiedStartupSendingReplyInfo)
-			.substateOf(State.IdentifiedStartup)
-			.onEntry(new Action() {
-				public void doIt() {
-					sendReplyInfo();
-				}})
-			.permit(Trigger.ExpectGo, State.IdentifiedStartupReceiveGo);
-		sessionConnectionConfig.configure(State.IdentifiedStartupReceiveGo)
-			.substateOf(State.IdentifiedStartup)
-			.permit(Trigger.SendBye, State.IdentifiedStartupSendingBye)
-			.permit(Trigger.ReceivedGo, State.IdentifiedStartupSendingGoToo);
-		sessionConnectionConfig.configure(State.IdentifiedStartupSendingGoToo)
-			.substateOf(State.IdentifiedStartup)
-			.onEntry(new Action() {
-				public void doIt() {
-					sendGoToo();
-					stateMachine.fire(Trigger.Connect);
-				}})
-			.permit(Trigger.Connect, State.EncryptedConnected);
 
 		return sessionConnectionConfig;
 	}
